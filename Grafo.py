19e79b18143cf28c8984f39e0df8b5fd51fc28db
fgsdt1 <62898318+fgsdt1@users.noreply.github.com>
2020-04-28 11:31:39 +0200

    Clase Grafo

--- Grafo.py
+++ Grafo.py
@@ -2,6 +2,7 @@
 # Clase Grafo que constará de un diccionario de vértices (los nodos) y las aristas que son las conexiones entre ellos
 # Las aristas se guardarán en una lista asociada al vertice y llevarán una ponderación
 import Vertice
+import math
 
 class Grafo():
 
@@ -10,6 +11,7 @@ class Grafo():
         self.listaVertices = {}
         self.size = 0
 
+    # Añade un vértice al grafo
     def addVertice(self, nombrevertice):
 
         # Si no existe el vertice con ese código se crea y se añade, en caso contrario se devuelve Null
@@ -21,6 +23,7 @@ class Grafo():
         else:
             return None
 
+    # Borra un vértice y todas las aristas relacionadas
     def deleteVertice(self, nombrevertice):
 
         # Se pregunta si el vertice existe para evitar errores. Si es así se borra
@@ -35,6 +38,7 @@ class Grafo():
 
         return
 
+    # Añade una arista entre dos vértices
     def addArista(self, verticede, verticea, ponderacion):
 
         # Si los vertices "de" o "a" no existen, se crean
@@ -48,6 +52,57 @@ class Grafo():
         self.listaVertices[verticede].unirVecino(verticea, ponderacion)
 
         return
+
+    # Calcula el camino menos costos (más corto) para ir de un punto a todos los demás verices usando el agoritmo Dijkstra
+    def shortestGrid (self, verticede):
+
+        # Creamos un diccionario de vertices (tablaVertices) con doble valor:
+        #       El tamaño del camino (inicializado a infinito) y el camino optimo para llegar ("")
+        #  y también creamos un diccionario de vértices (visitaVertices) que no se han visitado
+        tablaVertices = {}
+        visitaVertices = {}
+        for ver in self.listaVertices.keys():
+            tablaVertices[ver]=[math.inf, ""]
+            visitaVertices[ver]=False
+
+        # Aplicamos valores iniciales al vector origen
+        tablaVertices[verticede] = [0, verticede]
+        # Repetimos el proceso mientras que queden vertices por analizar (VistaVertices)
+        while len(visitaVertices) > 0:
+            # Fijamos el puntero en el nodo con menor distnacia y que este en la lista de vertices por visitar (visitaVertices)
+            # Para ello recorremos el diccionario. La primera vez será el nodo verticede, que es el único con valor 0
+            minVertice = ""
+            for ver in visitaVertices.keys():
+                if minVertice == "":
+                    minVertice = ver
+                else:
+                    if tablaVertices[ver][0] < tablaVertices[minVertice][0]:
+                        minVertice = ver
+
+            # borramos el puntero de la lista de visita
+            del visitaVertices[minVertice]
+
+            # Buscamos todas las aristas asociadas al mínimo vértice
+            for ari in self.listaVertices[minVertice].aristas.keys():
+                # Procesamos aquellas que no han sido procesadas todavía (están en a lista de visita)
+                if ari in visitaVertices.keys():
+                    # Calculamos las distancias. Distancia actual al nodo puntero más la ponderación de la arista
+                    distancia = tablaVertices[minVertice][0] + self.listaVertices[minVertice].aristas[ari]
+                    # Si la distancia es menor que la registrada en la tabla de vertices la actualizamos,
+                    if distancia < tablaVertices[ari][0]:
+                        tablaVertices[ari][0] = distancia
+                        # y actualizamos también el camino
+                        tablaVertices[ari][1] = tablaVertices[minVertice][1] + ari
+
+        # Salimos del bucle cuando están ya todos los nodos visitados. Se habrá actualizado la tabla de Vertices
+        # Devolvemos la tabla de vertices completa con el camino
+        return tablaVertices
+
+    # Calcula el camino menos costos (más corto) para ir de un punto a otro concreto usando el agoritmo Dijkstra
+    def shortestPath (self, verticede, verticea):
+
+        tablaVertices = self.shortestGrid(verticede)
+        return tablaVertices[verticea]
 
     def __str__(self):
 
--- GrafoTest.py
+++ GrafoTest.py
@@ -1,25 +1,32 @@
 import Grafo
 
+
 myGrafo = Grafo.Grafo()
 
-myGrafo.addVertice("1")
-myGrafo.addVertice("2")
-myGrafo.addVertice("3")
-myGrafo.addVertice("4")
+myGrafo.addVertice("A")
+myGrafo.addVertice("B")
+myGrafo.addVertice("C")
+myGrafo.addVertice("D")
+myGrafo.addVertice("E")
 
-myGrafo.listaVertices["1"].unirVecino("2", 4)
-myGrafo.listaVertices["2"].unirVecino("1", 4)
-myGrafo.listaVertices["2"].unirVecino("3", 2)
-myGrafo.listaVertices["3"].unirVecino("1", 1)
-myGrafo.listaVertices["3"].unirVecino("4", 4)
-myGrafo.listaVertices["4"].unirVecino("1", 1)
+myGrafo.listaVertices["A"].unirVecino("B", 3)
+myGrafo.listaVertices["A"].unirVecino("C", 1)
+myGrafo.listaVertices["B"].unirVecino("C", 7)
+myGrafo.listaVertices["B"].unirVecino("D", 5)
+myGrafo.listaVertices["B"].unirVecino("E", 1)
+#myGrafo.listaVertices["C"].unirVecino("A", 1)
+myGrafo.listaVertices["C"].unirVecino("B", 7)
+myGrafo.listaVertices["C"].unirVecino("D", 2)
+myGrafo.listaVertices["D"].unirVecino("B", 5)
+myGrafo.listaVertices["D"].unirVecino("C", 2)
+myGrafo.listaVertices["D"].unirVecino("E", 7)
+
 
 print(myGrafo)
 
-myGrafo.addArista("4", "1", 6)
-myGrafo.addArista("1", "4", 10)
+print(myGrafo.shortestGrid("B"))
+print(myGrafo.shortestPath("A", "B"))
+print(myGrafo.shortestPath("B", "A"))
 
-print(myGrafo)
 
-print(myGrafo.listaVertices["3"].devolverVecinos())
 
